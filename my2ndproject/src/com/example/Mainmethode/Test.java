e481afd73c63db3d7430c4b02c8b443ef9c91d91
anjulakade <anjulakadea145@gmail.com>
2024-05-11 16:23:34 +0530

    Update Test.java

--- my2ndproject/src/com/example/Mainmethode/Test.java
+++ my2ndproject/src/com/example/Mainmethode/Test.java
@@ -9,10 +9,16 @@ public class Test {
 		c.setId(1);
 		c.setName("mpgi");
 		c.setFees("80000");
+		Collage c1=new Collage();
+		c1.setId(1);
+		c1.setName("mpgi");
+		c1.setFees("80000");
 		System.out.println(c.getId());
 		System.out.println(c.getName());
 		System.out.println(c.getFees());
-				
+		System.out.println(c1.getId());
+		System.out.println(c1.getName());
+		System.out.println(c1.getFees());
 	}
 
 	public static void main(String[] args) {
